ab20f01c291289ce7ac0c8f2bbe338c227167a9e
mary.mastrion <mary.mastrion@student.atlantaclassical.org>
2026-02-20 14:12:42 -0500

    I am at school

--- Feb13.py
+++ Feb13.py
@@ -9,7 +9,34 @@ print(last_name)
 x = 10
 y = 5
 
+print(x - y)
+print(x * y)
+print(x / y)
 print(x + y)
 
+temperature = 72
+print(temperature > 65)
 
-15
+a = 8
+b = 12
+print(a < b)
+print (a = b)
+
+score = 85
+print(score > 90)
+
+is_raining = True 
+print(is_raining)
+
+has_ticket = True
+has_id = False
+
+is_closed = True
+print(not is_closed)
+
+name = Alice
+age= 30
+print(Alice is 30 years old)
+
+
+
